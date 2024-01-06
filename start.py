8ca265fc21fdb802e6433f1aab64ce28c93667b9
d1n2o3b4 <davebostons2017@gmail.com>
2024-01-06 18:45:15 +0000

    made changes

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, messagebox, PhotoImage, Label, StringVar, Entry
+from tkinter import Tk, messagebox, PhotoImage, Label, StringVar, Entry, Button
 
 app = Tk()
 
@@ -7,6 +7,10 @@ app.geometry("600x700+450+80")
 app.resizable(False,False)
 app.title("Voicer")
 app.configure(background="#4a4a4a")
+
+def Record():
+    freq = 44100
+    
 
 #icon
 img_icon = PhotoImage(file="./assets/record.png")
@@ -20,6 +24,9 @@ myimage.pack(pady=150)
 #entrybox
 duration = StringVar()
 entry = Entry(app,textvariable=duration,font="arial 20",width=10).pack(pady=10)
+
+#button
+record = Button(app,font="arial 20", text="Record", bg="#111111",fg="white",border=0).pack(pady=30)
 
 
 app.mainloop()
